3c41d68160bb73937924003f124cdd8109474415
True-Incarnatus <77661399+True-Incarnatus@users.noreply.github.com>
2021-01-23 03:49:12 +0800

    Update WerewolfGame.java
    Greetings. I'm William, @True_Incarnatus, a developer from the Kothar Labs. Today that I've completed the first prototype, and it might be one of the last few iterations. The Labs' kept quite busy prototyping the automated submarine which belongs to another project. In the sight of recent tests, I have a few issues to pinpoint. 
    First, the language generating mechanism needs great improvement: Sentences spoken by game-bots are not very logical and understandable. Gamebots are reported to sometimes elevate and debase the same player at the same time and repeat themselves numerous times. A logical analysis system is to be introduced in later iterations to avoid repetitive or conflictive statements.
    Second, strategy of gamebots playing civilians in game should be enhanced. According to our data, in 1000 trials, werewolves won for 993 times, while civilians only 7. As intensive analysis reveals, behavior of gamebot players during the day is rather random. They almost gamble around when they vote for banishment, which results in huge informational asymmetry of the two groups. It is obvious that werewolves, with much informational advantage, are much more likely to win the game. In later iterations, mechanisms should be introduced to make werewolves more "distinguishable" during days, like they somehow behave differently from civilians. Algorithm should also be improved to enable distinguishment of werewolves and civilians from speech.
    Last but not least, vital parts of the code lack comments, as a result of hasty completion. Developers are now working on comments and documentations. Through later iterations they'll be introduced and refined. 
    The good thing is, the first build is now running without reported runtime errors or exceptions during tests. That's the first milestone of the project achieved. Kothar Labs will make use of any room of improvements.

--- WerewolfGame.java
+++ WerewolfGame.java
@@ -3,24 +3,40 @@ import java.util.ArrayList;
 public class WerewolfGame{
     static Scanner sc=new Scanner(System.in);
     static Entity[] players;
+    static final int SIZE=9;
     static int VillagersLeft=3;
     static int WerewolvesLeft=3;
     static int SocerersLeft=3;
-    static int[] toll=new int[players.length];
+    static Witch witch=new Witch("Leary");
+    static Prophet prophet=new Prophet("Nazca");
+    static int[] toll=new int[SIZE];
     static int SOL=0;
     static boolean isNight=true;
-    static ArrayList<Entity> JSEntities;
+    static int copPos;
+    static Entity NightKill;
+    static ArrayList<Entity> JSEntities=new ArrayList<Entity>();
+    static ArrayList<Entity> suspended=new ArrayList<Entity>();
     static int randInt(int begin,int end){
         double key=Math.random();
         return((int)(key*(end-begin)+begin));
     }
+    static int getMin(int[] arr){
+        int min=65535;
+        for(int i=0;i<arr.length;i++){
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+        return min;
+    }
     public static void ini(){
+        players=new Entity[SIZE];
         players[0]=new Entity("William");
         players[1]=new Entity("Jack");
-        players[2]=new Entity();
-        players[3]=new Witch("Sara");
-        players[4]=new Prophet();
-        players[5]=new Hunter();
+        players[2]=new Entity("Tom");
+        players[3]=witch;
+        players[4]=prophet;
+        players[5]=new Hunter("Daisuke");
         players[6]=new Werewolf("IronMan");
         players[7]=new Werewolf("Wolverine");
         players[8]=new Werewolf("CaptainAmerica");
@@ -43,18 +59,18 @@ public class WerewolfGame{
         iniToll();
     }
     public static void iniToll(){
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             toll[i]=0;
         }
     }
     public static Entity pickImposter(){
         iniToll();
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             players[i].vote();
         }
         int max=-1;
         int maxPos=0;
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             if(toll[i]>max){
                 max=toll[i];
                 maxPos=i;
@@ -62,42 +78,43 @@ public class WerewolfGame{
         }
         return players[maxPos];
     }
-    public static void pickCop(){
-        boolean[] isCandid=new boolean[players.length];
+    public static String pickCop(){
+        boolean[] isCandid=new boolean[SIZE];
         boolean aflag=false;
         //Indicates if cop is chosen.
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             isCandid[i]=(players[i].getActiveness()>=140);
             if(isCandid[i]){
-                aflag=!aflag;
+                aflag=true;
             }
         }
         if(!aflag){
-            isCandid[randInt(0,players.length)]=true;
+            isCandid[randInt(0,SIZE)]=true;
             //If no candidate is selected through evaluating activeness, one will be chosen at random.
         }
         iniToll();
-        int[] candid=new int[players.length];
-        for(int i=0;i<players.length;i++){
+        int[] candid=new int[SIZE];
+        for(int i=0;i<SIZE;i++){
             if(isCandid[i]){
                 candid[i]=players[i].getCopVoteScore();
             }
         }
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             if(!isCandid[i]){
                 players[i].copVote(candid);
             }
         }
         int max=-1;
         int maxPos=0;
-        for(int i=0;i<players.length;i++){
+        for(int i=0;i<SIZE;i++){
             if(toll[i]>max){
                 maxPos=i;
                 max=toll[i];
             }
         }
         players[maxPos].isPolice=true;
-
+        copPos=maxPos;
+        return players[maxPos].name;
     }
     static class Judgement{
         Entity opr;
@@ -140,10 +157,10 @@ public class WerewolfGame{
         public String toString(){
             String temp=getRandPre();
             if(this.att){
-                temp+=getRandPosit().replaceAll("(.*)<arg>(.*)",this.opr.name);
+                temp+=getRandPosit().replace("<arg>",this.opr.name);
             }
             else{
-                temp=getRandNeg().replaceAll("(.*)<arg>(.*)",this.opr.name);
+                temp=getRandNeg().replace("<arg>",this.opr.name);
             }
             return temp;
         }
@@ -159,6 +176,7 @@ public class WerewolfGame{
         protected String ident;
         protected ArrayList<Judgement> infoList;
         public Entity(String name){
+            this.isAlive=true;
             this.reput=127;
             this.name=name;
             this.ident="Villager";
@@ -189,9 +207,12 @@ public class WerewolfGame{
         public int getCopVoteScore(){
             return this.getRep()+(int)(this.acti*0.5)+(int)(this.skill*0.8);
         }
-        public void genRandJudge(){
-            int[] impres=new int[players.length];
-            for(int i=0;i<players.length;i++){
+        public void noiseJudge(){
+            if(!this.isAlive){
+                return;
+            }
+            int[] impres=new int[SIZE];
+            for(int i=0;i<SIZE;i++){
                 if(players[i].equal(this)){
                     continue;
                 }
@@ -199,10 +220,39 @@ public class WerewolfGame{
             }
             int key=(int)((this.getActiveness()-130)/10);
             for(int i=0;i<key;i++){
-                
+                this.genRandJudge(true);
+                this.genRandJudge(false);
             }
         }
+        protected void genRandJudge(boolean flag){
+            int[] places=new int[SIZE];
+            int[] impres=new int[SIZE];
+            int blanks=0;
+            for(int i=0;i<SIZE;i++){
+                if(!players[i].isAlive){
+                    blanks++;
+                    continue;
+                }
+                places[i]=i+1;
+                impres[i]=players[i].getRep();
+            }
+            int temp;
+            int temp2;
+            for(int i=0;i<SIZE;i++){
+                for(int j=i;j<SIZE;j++){
+                    if(impres[i]<impres[j]){
+                        temp=impres[i]; impres[i]=impres[j]; impres[j]=temp;
+                        temp2=places[i]; places[i]=places[j]; places[j]=temp2;
+                    }
+                }
+            }
+            int judgeKey=Integer.min(Integer.min(randInt(0,SIZE-blanks),randInt(0,SIZE-blanks)),randInt(0,SIZE-blanks));
+            this.infoList.add(new Judgement(players[places[judgeKey]-1],flag));
+        }
         public void speak(){
+            if(!this.isAlive){
+                return;
+            }
             for(int i=0;i<this.infoList.size();i++){
                 System.out.print(this.infoList.get(i).toString());
                 if(this.infoList.get(i).att){
@@ -212,9 +262,10 @@ public class WerewolfGame{
                     this.debase(this.infoList.get(i).opr);
                 }
             }
+            this.infoList.clear();
         }
         public void vote(){
-            int[] impressions=new int[players.length];
+            int[] impressions=new int[SIZE];
             for(int i=0;i<impressions.length;i++){
                 if(!players[i].isAlive){
                     continue;
@@ -241,7 +292,7 @@ public class WerewolfGame{
 
         }
         public void copVote(int[] scores){
-            int[] impres=new int[players.length];
+            int[] impres=new int[SIZE];
             for(int i=0;i<scores.length;i++){
                 if(!players[i].isAlive){
                     continue;
@@ -268,6 +319,7 @@ public class WerewolfGame{
         
     }
     static class Werewolf extends Entity{
+        static ArrayList<Entity> targets;
         public Werewolf(String s){
             super(s);
             this.ident="Werewolf";
@@ -275,6 +327,18 @@ public class WerewolfGame{
             if(this.JSstate){
                 this.acti+=10;
             }
+        }
+        static String kill(){
+            if(targets==null){
+                targets=Werewolf.acqTar();
+            }
+            int[] candPos=new int[5];
+            for(int i=0;i<5;i++){
+                candPos[i]=randInt(0,Werewolf.targets.size());
+            }
+            suspended.add(Werewolf.targets.get(0));
+            Werewolf.targets.remove(0);
+            return suspended.get(suspended.size()-1).name;
         }
         public void jumpscare(){
             if(!this.JSstate){
@@ -286,11 +350,14 @@ public class WerewolfGame{
             JSEntities.add(this);
         }
         public void speak(){
+            if(!this.isAlive){
+                return;
+            }
             jumpscare();
             super.speak();
         }
         public void copVote(){
-            int[] impres=new int[players.length];
+            int[] impres=new int[SIZE];
             for(int i=0;i<impres.length;i++){
                 if(!players[i].isAlive){
                     continue;
@@ -321,23 +388,266 @@ public class WerewolfGame{
                     return;
                 }
             }
-            toll[randInt()]
+            toll[randInt(0,SIZE)]++;
+        }
+        public static ArrayList<Entity> acqTar(){
+            ArrayList<Entity> candid=new ArrayList<Entity>();
+            for(int i=0;i<JSEntities.size();i++){
+                if(JSEntities.get(i).ident!="Werewolf"){
+                    candid.add(JSEntities.get(i));
+                }
+            }
+            for(int i=0;i<SIZE;i++){
+                if(players[i].ident!="Werewolf"&&!candid.contains(players[i])){
+                    candid.add(players[i]);
+                }
+            }
+            return candid;
+        }
+        public void die(){
+            WerewolvesLeft--;
+            this.isAlive=!isAlive;
         }
     }
     static class Witch extends Entity{
-
+        public boolean hasHealingPotion,hasPoisonPotion;
+        private boolean signal=false; //False for idle, true for occupied;
+        public Witch(String s){
+            super(s);
+            this.acti+=10;
+            this.ident="Witch";
+            hasHealingPotion=true;
+            hasPoisonPotion=true;
+        }
+        public void poison(int num){
+            this.hasPoisonPotion=false;
+            suspended.add(players[num]);
+        }
+        public void heal(){
+            this.hasHealingPotion=false;
+            suspended.remove(0);
+        }
+        public String randPois(){
+            if(!this.hasPoisonPotion || !this.isAlive || this.signal){
+                return " ";
+            }
+            else{
+                this.signal=true;
+                for(int i=0;i<SIZE;i++){
+                    if(players[i].getRep()<=60){
+                        this.poison(i);
+                        return players[i].name;
+                    }
+                }
+            }
+            return "";
+        }
+        public boolean randHeal(){
+            if(!this.hasHealingPotion || !this.isAlive || this.signal){
+                return false;
+            }
+            this.signal=true;
+            if(suspended.get(0).JSstate && suspended.get(0).getRep()>160){
+                this.heal();
+            }
+            return true;
+        }
+        public void die(){
+            SocerersLeft--;
+            this.isAlive=!isAlive;
+        }
     }
     static class Hunter extends Entity{
-
+        public Hunter(String s){
+            super(s);
+        }
+        public String randShoot(){
+            int minImpres=1000;
+            int minPos=-1;
+            for(int i=0;i<SIZE;i++){
+                int temp=players[i].getRep();
+                if(temp<minImpres){
+                    minImpres=temp;
+                    minPos=i;
+                }
+            }
+            players[minPos].die();
+            return players[minPos].name;
+        }
+        public void die(){
+            SocerersLeft--;
+            this.randShoot();
+            this.isAlive=!this.isAlive;
+        }
     }
     static class Prophet extends Entity{
+        boolean[] hasRevealed,revIdents;
         public Prophet(String s){
             super(s);
+            this.JSstate=true;
             this.acti+=20;
+            this.ident="Prophet";
+            this.hasRevealed=new boolean[SIZE];
+            this.revIdents=new boolean[SIZE];
+        }
+        public void jumpscare(){
+            if(!JSEntities.isEmpty()){
+                System.out.print("Despites ");
+                if(JSEntities.size()==1){
+                    System.out.print(JSEntities.get(0).name+" ");
+                }
+                else if(JSEntities.size()==2){
+                    System.out.print(JSEntities.get(0).name+" and "+JSEntities.get(1).name);
+                }
+                else{
+                    System.out.print("some other people ");
+                }
+                System.out.print("claim(s) to be prophets, ");
+            }
+            System.out.print("I am the only real prophet in this game.");
+            for(int i=0;i<JSEntities.size();i++){
+                this.infoList.add(new Judgement(JSEntities.get(i),false));
+            }
+            JSEntities.add(this);
+        }
+        public void speak(){
+            if(!this.isAlive){
+                return;
+            }
+            this.jumpscare();
+            super.speak();
+        }
+        public boolean reveal(int num){ // Returns true for a human, false for a werewolf.
+            // True for humans, false for werewolves.
+            boolean flag=(players[num].ident=="Werewolf");
+            this.infoList.add(new Judgement(players[num],flag));
+            this.hasRevealed[num]=true;
+            this.revIdents[num]=flag;
+            return flag;            
+        }
+        public Entity randReveal(){
+            int[] impres=new int[SIZE];
+            int blanks=0;
+            for(int i=0;i<SIZE;i++){
+                if(!players[i].isAlive || this.hasRevealed[i]){
+                    blanks++;
+                    continue;
+                }
+                impres[i]=players[i].getRep();
+            }
+            int temp;
+            for(int i=0;i<impres.length;i++){
+                for(int j=i;j<impres.length;j++){
+                    if(impres[i]>impres[j]){
+                        temp=impres[i];impres[i]=impres[j];impres[j]=temp;
+                    }
+                }
+            }
+            int pos=Integer.max(Integer.max(randInt(blanks,impres.length),randInt(blanks,impres.length)),randInt(blanks,impres.length));
+            
+            String[] ret=new String[2];
+            ret[0]=players[pos].name;
+            boolean flag=this.reveal(pos);
+            if(flag){
+                ret[1]="Human";
+            }
+            else{
+                ret[1]="Werewolf";
+            }
+            return ret;
+        }
+
+        public void die(){
+            SocerersLeft--;
+            this.isAlive=!isAlive;
         }
 
     }
-    public static void main(String[] args){
+    public static int gamestate(){
+        if(SocerersLeft==0 || VillagersLeft==0){
+            return 1;
+        }
+        else if(WerewolvesLeft==0){
+            return 2;
+        }
+        else{
+            return 0;
+        }
+    }
+    public static void doNight(){
+        System.out.println("--------Night "+SOL+"--------");
+        isNight=true;
+        if(prophet.isAlive){
+            String[] stash=prophet.randReveal();
+            System.out.println("The prophet revealed identity of "+stash[0]+", who is a "+stash[1]);
+        }
+        System.out.println("Werewolves attempted to kill"+Werewolf.kill());
+        if(witch.randHeal()){
+            System.out.println("The witch used healing potion.");
+        }
+        else{
+            System.out.println("The witch didn't use healing potion.");
+        }
+        String tempstr=witch.randPois();
+        switch(tempstr){
+            case(" "):{
+                break;
+            }
+            case(""):{
+                System.out.println("The witch didn't use poisoning potion.");
+                break;
+            }
+            default:{
+                System.out.println("The witch poisoned"+tempstr);
+            }
+        }
+    }
+    public static void doDay(){
+        SOL++;
+        System.out.println("--------Day "+SOL+"--------");
+        if(SOL==1){
+            System.out.println("Congratulations, "+pickCop()+" was elected as the sheriff.");
+        }
+        for(int i=0;i<suspended.size();i++){
+            System.out.println("Notice: Last night "+suspended.get(i).name+" was killed.");
+            suspended.get(i).die();
+        }
+        suspended.clear();
+        if(gamestate()!=0){
+            return;
+        }
+        for(int i=0;i<SIZE;i++){
+            players[i].noiseJudge();
+        }
+        if(Math.random()>0.5){
+            for(int i=copPos+1;i<SIZE+copPos+1;i++){
+                System.out.print(players[i%SIZE].name+": ");
+                players[i%SIZE].speak();
+                System.out.println();
+            }
+        }
+        else{
+            for(int i=copPos+SIZE-1;i>copPos-1;i--){
+                System.out.print(players[i%SIZE].name+": ");
+                players[i%SIZE].speak();
+                System.out.println();
+            }
+        }
+        Entity exec=pickImposter();
+        System.out.println(exec.name+" was banished through ostracism.");
 
+    }
+    public static void main(String[] args){
+        ini();
+        while(gamestate()==0){
+            doNight();
+            doDay();
+        }
+        if(gamestate()==1){
+            System.out.println("----****Werewolves Win****----");
+        }
+        else if(gamestate()==2){
+            System.out.println("----____Humans Win____----");
+        }
     }
 }
